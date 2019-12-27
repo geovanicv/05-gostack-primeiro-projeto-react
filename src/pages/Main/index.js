30afd6a2e547b7757d38c5306c6ab42dd26d9308
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2019-12-27 16:18:55 -0300

    Estilizando pagina main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa'
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
   return (
-    <Title /*error = {true}*/>
-      Hello World
-    </Title>
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input
+          type="text"
+          placeholder="Adicionar repositorio" />
+
+        <SubmitButton disable>
+          <FaPlus color="#FFF" size={14}/>
+        </SubmitButton>
+      </Form>
+
+
+    </Container>
   )
 }
